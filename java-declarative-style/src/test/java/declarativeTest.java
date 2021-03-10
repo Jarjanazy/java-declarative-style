1a1731a88c41a41dfd2aaa4b062b086a9ae38c37
jarjanazy <celil.cercenazi@gmail.com>
2021-03-10 20:02:00 +0300

    Add a test for an imperative and declarative case

--- java-declarative-style/src/test/java/declarativeTest.java
+++ java-declarative-style/src/test/java/declarativeTest.java
@@ -1,3 +1,35 @@
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import static java.lang.Character.isDigit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class declarativeTest {
+    @Test
+    void test(){
+        List<String> strings = Arrays.asList("Hello", "2No", "Hola", "1Dear", "Why", "Mars_Planet");
+
+        assertEquals(12, getResult_imperative(strings));
+        assertEquals(12, getResult_Declarative(strings));
+    }
+
+    private int getResult_imperative(List<String> strings) {
+        int result = 0;
+        for (String string : strings){
+            if(isDigit(string.charAt(0))) continue;
+            if (string.contains("_")) continue;
+            result += string.length();
+        }
+        return result;
+    }
+
+    private int getResult_Declarative(List<String> strings){
+        return strings.
+                stream().
+                filter(s -> !isDigit(s.charAt(0))).
+                filter(s -> !s.contains("_")).
+                mapToInt(String::length).
+                sum();
+    }
+
 }
